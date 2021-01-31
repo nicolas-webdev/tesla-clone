b9fd131bb69914ce389a933d7c218c288221473b
Nicolas <nicobsas+webdev@gmail.com>
2021-01-31 23:21:01 +0900

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>hi</h1>
+      <Header />
+      {/* header */}
+      {/* items . snapping */}
+      {/* items . snapping */}
+      {/* items . snapping */}
+      {/* items . snapping */}
     </div>
   );
 }
